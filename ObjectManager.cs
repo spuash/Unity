68794d9d7b63158f15f2288cbdeb659b059c3bd8
spuash <spuash.minecraft@gmail.com>
2017-11-10 00:24:52 +0900

    Add files via upload

---
+++ ObjectManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour {
+
+	void Start () {
+		//Game Objectを破棄しないようにする
+		DontDestroyOnLoad (gameObject);
+	}
+
+	public void DestroyObject(){
+		Destroy (gameObject);
+	}
+
+}
